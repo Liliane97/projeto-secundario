24bcb6a0f914fb78dbb2e09b2148ef20f4ad93ef
Liliane Basílio <liliane.basilio@softagon.com.br>
2023-06-26 11:45:01 -0300

    alterando nuxt.config e package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const softagon = {
+  dark: false,
+  colors: {
+    background: '#1e293b',
+    surface: '#FFFFFF',
+    primary: '#fe682d',
+    'primary-darken-1': '#3700B3',
+    secondary: '#00a3d7',
+    'secondary-darken-1': '#018786',
+    error: '#fe682d',
+    info: '#2196F3',
+    success: '#01b9e0',
+    warning: '#fea800',
+  },
+}
+
 export default defineNuxtConfig({
-  modules: [
-    '@invictus.codes/nuxt-vuetify'
+  ssr: true,
+  build: {
+    transpile: ['vuetify'],
+  },
+   modules: [
+    '@invictus.codes/nuxt-vuetify',
   ],
   vuetify: {
-    /* vuetify options */
     vuetifyOptions: {
-      // @TODO: list all vuetify options
+      theme: {
+        defaultTheme: 'softagon',
+        themes: {
+          softagon,
+        },
+      },
+      defaults: {
+       
+      },
+      icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
     },
-
     moduleOptions: {
-      /* nuxt-vuetify module options */
-      treeshaking: true ,
-      useIconCDN: true ,
-
-      /* vite-plugin-vuetify options */
-      // styles: true | 'none' | 'expose' | 'sass' | { configFile: string },
-      autoImport: true ,
-    }
-  }
+      treeshaking: true,
+      useIconCDN: true,
+      styles: 'sass',
+      autoImport: true,
+    },
+  },
+  //   nitro: {
+  //   serveStatic: true,
+  //   compressPublicAssets: true,
+  // },
+ 
 })
